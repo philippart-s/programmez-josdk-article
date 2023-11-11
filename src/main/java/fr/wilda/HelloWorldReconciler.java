83945208a8542de4ab0bb57ffd3d47608ad0eec2
Stéphane Philippart <wildagsx@gmail.com>
2023-11-11 13:42:54 +0100

    Hello world (#2)
    * feat: ✨ Local execution
    
    * doc: 📝 add used commands
    
    * feat: ✨ Packaging & K8s deployment

---
+++ src/main/java/fr/wilda/HelloWorldReconciler.java
@@ -0,0 +1,34 @@
+package fr.wilda;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.javaoperatorsdk.operator.api.reconciler.Cleaner;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.DeleteControl;
+import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+
+public class HelloWorldReconciler implements Reconciler<HelloWorld>, Cleaner<HelloWorld>  { 
+  private static final Logger log = LoggerFactory.getLogger(HelloWorldReconciler.class);
+  private final KubernetesClient client;
+
+  public HelloWorldReconciler(KubernetesClient client) {
+    this.client = client;
+  }
+
+  @Override
+  public UpdateControl<HelloWorld> reconcile(HelloWorld resource, Context context) {
+    log.info("👋 Hello, World 🌏! From {}!", resource.getSpec().getName());
+
+    return UpdateControl.noUpdate();
+  }
+
+  @Override
+  public DeleteControl cleanup(HelloWorld resource, Context<HelloWorld> context) {
+    log.info("🥲  Goodbye, World 🌏! From {}!", resource.getSpec().getName());
+
+    return DeleteControl.defaultDelete();
+  }
+}
+
---
+++ src/main/java/fr/wilda/HelloWorldStatus.java
@@ -0,0 +1,6 @@
+package fr.wilda;
+
+public class HelloWorldStatus {
+
+    // Add Status information here
+}
